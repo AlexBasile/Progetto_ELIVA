5805d44c5ded520de960f39baa691760395a4abd
Tobia Giani <gianitobia@gmail.com>
2014-02-04 11:39:13 +0100

    Aggiunta visualizzazione dei risultati sift con subplot

--- test_sift.m
+++ test_sift.m
@@ -8,4 +8,13 @@ for index = 2:16
 end
 
 sim = SIFT_Similarity(Im1,Images);
+figure;
+subplot(4,4,1);
+imshow(Images{1, 1});
+title('Immagine originale');
+for index = 2:16
+    subplot(4,4,index);
+    imshow(Images{1, index-1});
+    title(strcat('Immagine ',num2str(index),'.jpg con sim = ',num2str(sim(index-1))));
+end
 sim
