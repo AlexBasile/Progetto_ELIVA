40a8473c2b51d5f3970d47944086eb5ce01de715
Tobia Giani <gianitobia@gmail.com>
2014-02-11 11:45:16 +0100

    varie prove di test_sift

--- test_sift.m
+++ test_sift.m
@@ -1,4 +1,4 @@
-
+clc; clear; close all;
 Im1 = imread('test_img/1.jpg');
 
 Images = cell(1,15);
@@ -11,10 +11,9 @@ sim = SIFT_Similarity(Im1,Images);
 figure;
 subplot(4,4,1);
 imshow(Im1);
-title('Immagine originale');
+title('Immagine originale "1.jpg"');
 for index = 2:16
     subplot(4,4,index);
     imshow(Images{1, index-1});
-    title(strcat('Immagine ',num2str(index),'.jpg con sim = ',num2str(sim(index-1))));
+    title(strcat('Immagine  "',num2str(index),'.jpg" con sim = ',num2str(sim(index-1))));
 end
-sim
