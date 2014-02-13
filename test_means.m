dd45da86641e7bafe092b26978c2b067bb8b3c66
Tobia Giani <gianitobia@gmail.com>
2014-02-13 16:10:15 +0100

    modifica a cazzo

--- test_means.m
+++ test_means.m
@@ -1,8 +1,10 @@
 clc; clear; close all;
 
 %carico le librerie in memoria
-run('vlfeat-0.9.17/toolbox/vl_setup')
+run('vlfeat-0.9.18/toolbox/vl_setup')
 
+num_cluster = 8;
+canny=0;
 
 Images = cell(1,16);
 for index = 1:16
@@ -15,13 +17,17 @@ for j = 1:16
     subplot(4,4,j);
     imshow(Images{1,j});
 end
+
 figure;
+titolo=strcat('Akula con num_cluster = ',num2str(num_cluster),' canny = ',num2str(canny));
+name(titolo);
 
 for i = 1:16
-    subplot(4,4,i);
+    subplot(2,4,mod(i,8)+1);
+
     imshow(Images{1,i});
-    [centers, assignments, fi, di] = SIFT_AKULA(Images{1,i});
-    [A, dA] = create_descriptor(centers, assignments, di,fi);
+    [centers, assignments, fi, di] = SIFT_AKULA(Images{1,i},canny,num_cluster);
+    [A, dA] = create_descriptor(centers, assignments);
     
     hold on
     for index = 1:size(centers,2)
@@ -29,14 +35,18 @@ for i = 1:16
         ind = find(assignments == index);
         plot(fi(1,ind), fi(2,ind), colors(index,:),'MarkerSize',25);
     end
-    
+    if i==8
+        figure;
+        title(titolo)
+    end
 end
-%{
+
 figure;
 for i = 1:16
-    subplot(4,4,i);
+    subplot(2,4,mod(i,8)+1);
     imshow(Images{1,i});
-    [centers, assignments, fi, di] = SIFT_AKULA(Images{1,i}, 1);
+    [centers, assignments, fi, di] = SIFT_AKULA(Images{1,i},canny,num_cluster);
+    [A, dA] = create_descriptor(centers, assignments,di);
     
     hold on
     for index = 1:size(centers,2)
@@ -44,6 +54,25 @@ for i = 1:16
         ind = find(assignments == index);
         plot(fi(1,ind), fi(2,ind), colors(index,:),'MarkerSize',25);
     end
-    
+    if i==8
+        figure;
+    end
 end
-%}
+
+figure;
+for i = 1:16
+    subplot(2,4,mod(i,8)+1);
+    imshow(Images{1,i});
+    [centers, assignments, fi, di] = SIFT_AKULA(Images{1,i},canny,num_cluster);
+    [A, dA] = create_descriptor(centers, assignments,di,fi);
+    
+    hold on
+    for index = 1:size(centers,2)
+        plot(centers(1,index), centers(2,index), colors(index,:),'MarkerSize',50);
+        ind = find(assignments == index);
+        plot(fi(1,ind), fi(2,ind), colors(index,:),'MarkerSize',25);
+    end
+    if i==8
+        figure;
+    end
+end
