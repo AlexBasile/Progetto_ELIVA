146864fe7168ce7df522d9a677d213e53c6e592f
Tobia Giani <gianitobia@gmail.com>
2014-02-16 23:32:56 +0100

    continuo aggiornamento

--- launcher_stats.m
+++ launcher_stats.m
@@ -9,12 +9,17 @@ run('vlfeat-0.9.18/toolbox/vl_setup')
     %%mode = 
     %% 0 se si vuol lanciare Akula in modo semplice
     %% 1 se si vuol lanciare Akula con la funzione di somiglianza che tenga 
-    %%conto anche dei descrittori dei cluster
+    %%conto anche dei descrittori dei cluster calcolati come una media
+    %% 2 se si vuol lanciare Akula con la funzione di somiglianza che tenga 
+    %%conto anche dei descrittori dei cluster calcolati attraverso la
+    %%pesatura deikeypoint facenti parte il cluster facendo attenzione a
+    %%diminuire i keypoint contesi tra i cluster
     mode = 0;
 
-    %%canny = se vale 0 verrá lanciato lo script con l'algoritmo base fornito
-    %%da Akula, se vale 1 verrá lanciato con l'algoritmo di Canny base, se vale
-    %%2 verrá lanciato con Canny + dilatazione dei bordi 
+    %%canny = 
+    %% 0 verrá lanciato lo script con l'algoritmo base fornito da Akula
+    %% 1 verrá lanciato con l'algoritmo di Canny base
+    %% 2 verrá lanciato con Canny + dilatazione dei bordi 
     canny=1;
 
     %%num_cluster = numero di cluster da ricercare nelle immagini
@@ -41,26 +46,44 @@ colors = ['b.';'m.';'c.';'r.';'g.';'w.';'y.';'k.'];
 
 figure;
 
-if mode
+if mode==0
     [centers1, assignments] = SIFT_AKULA(Images{1,sel_img},canny,num_cluster);
 else
     [centers1, assignments, fi, di] = SIFT_AKULA(Images{1,sel_img},canny,num_cluster);
 end;
 [centers_ord,ind1] = sort(centers1(1,:));
-if mode
-[A1] = create_descriptor(centers1(:,ind1), assignments);
+
+if mode == 0
+    [A1] = create_descriptor(centers1(:,ind1), assignments);
+elseif mode == 1
+    [A1, dA1] = create_descriptor(centers1(:,ind1), assignments, fi);
 else
-[A1, dA1] = create_descriptor(centers1(:,ind1), assignments);
+    [A1, dA1] = create_descriptor(centers1(:,ind1), assignments, fi,di);
 end;
 
 for i=1:num_images
     subplot(4,2,mod(i,8)+1);
     imshow(Images{1,i});
     
-    [centers2, assignments, fi, di] = SIFT_AKULA(Images{1,i},canny,num_cluster);
+    if mode==0
+        [centers2, assignments] = SIFT_AKULA(Images{1,sel_img},canny,num_cluster);
+    else
+        [centers2, assignments, fi, di] = SIFT_AKULA(Images{1,sel_img},canny,num_cluster);
+    end;
     [centers_ord,ind2] = sort(centers2(1,:));
-    [A2, dA2] = create_descriptor(centers2(:,ind2), assignments,di,fi);
-    [sim(i) d(i)] = AKULA_Sim( A1,A2,dA1,dA2 );
+
+    if mode == 0
+        [A2] = create_descriptor(centers2(:,ind2), assignments);
+    elseif mode == 1
+        [A2, dA2] = create_descriptor(centers2(:,ind2), assignments, fi);
+    else
+        [A2, dA2] = create_descriptor(centers2(:,ind2), assignments, fi,di);
+    end;
+    if mode==0
+        [sim(i)] = AKULA_Sim( A1,A2 );
+    else
+        [sim(i) d(i)] = AKULA_Sim( A1,A2,dA1,dA2 );
+    end
     
     hold on
     for index = 1:size(centers2,2)
@@ -76,5 +99,9 @@ end
 sim=1-sim / max(sim);
 d=1-d / max(d);
 
-l=sim*0.5+0.5*d;
+if mode == 0
+    l=sim;
+else
+    l=sim*0.5+0.5*d;
+end
 [l,i]=sort(l,'descend')
