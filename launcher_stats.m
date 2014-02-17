ab6c741753dc9cd388f2639ae69d4d7f1b26bc65
Tobia Giani <gianitobia@gmail.com>
2014-02-17 16:42:19 +0100

    finito launcher

--- launcher_stats.m
+++ launcher_stats.m
@@ -27,12 +27,16 @@ run('vlfeat-0.9.18/toolbox/vl_setup')
 
     %%sel_img = identificativo dell'immagine da prendere come query per il
     %%programma
-    sel_img=16;
+    sel_img=1;
 
     %%num_images = numero totale delle immagini presente nella cartella
     %%test_img
     num_images = 32;
-
+    %%num_iter = numero totale delle iterazioni
+    num_iter = 4;
+    %%enable_plot = 0/1
+    enable_plot = 0;
+    
 %%% Fine variabili
 %%%
 
@@ -61,52 +65,63 @@ else
     [A1, dA1] = create_descriptor(centers1(:,ind1), assignments1, di1,fi1);
 end;
 
-sim=zeros(1,num_images);
-d=zeros(1,num_images);
+l=zeros(num_iter,num_images);
+parfor iter = 1:num_iter
+    sim=zeros(1,num_images);
+    d=zeros(1,num_images);
+    for i=1:num_images
+        if enable_plot
+            subplot(4,2,mod(i,8)+1);
+            imshow(Images{1,i});
+        end
+        
+        if mode==0
+            [centers2, assignments2] = SIFT_AKULA(Images{1,i},canny,num_cluster);
+        else
+            [centers2, assignments2, fi2, di2] = SIFT_AKULA(Images{1,i},canny,num_cluster);
+        end;
+        [centers_ord,ind2] = sort(centers2(1,:));
 
-for i=1:num_images
-    subplot(4,2,mod(i,8)+1);
-    imshow(Images{1,i});
-    
-    if mode==0
-        [centers2, assignments2] = SIFT_AKULA(Images{1,i},canny,num_cluster);
-    else
-        [centers2, assignments2, fi2, di2] = SIFT_AKULA(Images{1,i},canny,num_cluster);
-    end;
-    [centers_ord,ind2] = sort(centers2(1,:));
+        if mode == 0
+            [A2] = create_descriptor(centers2(:,ind2), assignments2);
+        elseif mode == 1
+            [A2, dA2] = create_descriptor(centers2(:,ind2), assignments2, di2);
+        else
+            [A2, dA2] = create_descriptor(centers2(:,ind2), assignments2, di2,fi2);
+        end;
+        if mode==0
+            [sim(i)] = AKULA_Sim( A1,A2 );
+        else
+            [sim(i), d(i)] = AKULA_Sim( A1,A2,dA1,dA2 );
+        end
 
-    if mode == 0
-        [A2] = create_descriptor(centers2(:,ind2), assignments2);
-    elseif mode == 1
-        [A2, dA2] = create_descriptor(centers2(:,ind2), assignments2, di2);
-    else
-        [A2, dA2] = create_descriptor(centers2(:,ind2), assignments2, di2,fi2);
-    end;
-    if mode==0
-        [sim(i)] = AKULA_Sim( A1,A2 );
-    else
-        [sim(i), d(i)] = AKULA_Sim( A1,A2,dA1,dA2 );
-    end
-    
-    hold on
-    for index = 1:size(centers2,2)
-        plot(centers2(1,index), centers2(2,index), colors(index,:),'MarkerSize',50);
-        if mode ~= 0
-            ind = find(assignments2 == index);
-            plot(fi2(1,ind), fi2(2,ind), colors(index,:),'MarkerSize',25);
+        hold on
+        if enable_plot
+            for index = 1:size(centers2,2)
+                plot(centers2(1,index), centers2(2,index), colors(index,:),'MarkerSize',50);
+                if mode ~= 0
+                    ind = find(assignments2 == index);
+                    plot(fi2(1,ind), fi2(2,ind), colors(index,:),'MarkerSize',25);
+                end
+            end
+        end
+        
+        if mod(i,8)==0 && i~=num_images && enable_plot
+            figure;
         end
     end
-    if mod(i,8)==0 && i~=num_images
-        figure;
+
+
+    sim=1-sim / max(sim);
+    d=1-d / max(d);
+
+    if mode == 0
+        l(iter,:)=sim;
+    else
+        l(iter,:)=sim*0.5+0.5*d;
     end
 end
+r=sum(l)/num_iter;
 
-sim=1-sim / max(sim);
-d=1-d / max(d);
-
-if mode == 0
-    l=sim;
-else
-    l=sim*0.5+0.5*d;
-end
-[l,i]=sort(l,'descend')
+%[r, indici]=sort(results,'descend');
+risultati=[risultati;r];
